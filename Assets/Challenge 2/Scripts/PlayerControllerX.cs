5d437d6c961b51938938b08feff445d9f9393d2f
Gentlemat <mateusz.rosiek2020@gmail.com>
2024-02-01 15:39:52 +0100

    Initial commit

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    public float cooldown = 1f; 
+    private float cooldownTimer = 1f; 
+
+    // Update is called once per frame
+    void Update()
+    {
+        cooldownTimer -= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && cooldownTimer < 0f)
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            cooldownTimer = cooldown;
+        }
+    }
+}
